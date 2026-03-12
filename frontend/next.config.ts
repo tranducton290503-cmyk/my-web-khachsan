dd241b1a8a60079c9126a972cf5d23775d5c2903
tranducton290503-cmyk <tranducton290503@gmail.com>
2026-03-12 16:18:16 +0700

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Thêm dòng này để Next.js tạo ra thư mục 'out' chứa index.html
+  images: {
+    unoptimized: true, // Bắt buộc phải có dòng này khi dùng Static Site
+  },
 };
 
 export default nextConfig;
