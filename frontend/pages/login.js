8bd16b39206ab34f8a5c56730a431bb3c4bb6ed6
tranducton290503-cmyk <tranducton290503@gmail.com>
2026-03-12 16:43:35 +0700

    Update login.js

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -14,7 +14,7 @@ export default function Login() {
     setError("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("https://my-web-khachsan.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
